e1a14163004f3ad3e79fc389746abe99c340fc23
Raphinha7 <raphael.cruz@iesb.edu.br>
2026-06-13 16:10:48 -0300

    cria formulario e validacoes

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,5 +1,101 @@
+import { useState, useContext } from "react";
+import { UsuarioContext } from "../context/UsuarioContext";
+
 function Cadastro() {
-  return <h1>Cadastro</h1>;
+  const { usuarios, setUsuarios } = useContext(UsuarioContext);
+
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [telefone, setTelefone] = useState("");
+
+  function cadastrar(e) {
+    e.preventDefault();
+
+  
+    if (!nome.trim()) {
+      alert("Nome obrigatório");
+      return;
+    }
+
+    if (!email.includes("@")) {
+      alert("Email inválido");
+      return;
+    }
+
+    if (!telefone.trim()) {
+      alert("Telefone obrigatório");
+      return;
+    }
+
+    
+    const novoUsuario = {
+      id: Date.now(),
+      nome,
+      email,
+      telefone,
+    };
+
+   
+    setUsuarios([...usuarios, novoUsuario]);
+
+   
+    setNome("");
+    setEmail("");
+    setTelefone("");
+
+    alert("Usuário cadastrado com sucesso!");
+  }
+
+  return (
+    <div className="container">
+      <h1>Cadastro de Usuários</h1>
+
+      <form onSubmit={cadastrar}>
+        <div>
+          <label>Nome:</label>
+          <br />
+          <input
+            type="text"
+            placeholder="Digite o nome"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Email:</label>
+          <br />
+          <input
+            type="email"
+            placeholder="Digite o email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Telefone:</label>
+          <br />
+          <input
+            type="text"
+            placeholder="Digite o telefone"
+            value={telefone}
+            onChange={(e) => setTelefone(e.target.value)}
+          />
+        </div>
+
+        <br />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Cadastro;
